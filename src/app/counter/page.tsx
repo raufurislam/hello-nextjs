48650ab7d25b67d6aa81285bb84fc5c7daeadb46
raufurislam <raufurislam@gmail.com>
2025-09-18 12:57:37 +0600

    dive deep into React server components

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,10 @@
+import Counter from "@/components/Counter";
+
+export default function CounterPage() {
+  console.log("Hello from Counter routes");
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
