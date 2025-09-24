b53bb12b88d3c247bc938a3dc8b7fc32b5155a17
raufurislam <raufurislam@gmail.com>
2025-09-24 23:49:35 +0600

    Random

--- src/app/(.)intercepting/page.tsx
+++ src/app/(.)intercepting/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <div>
-      this is intercepting Page
+      <h1>this is intercepting Page</h1>
       <div className="w-32 h-32 bg-amber-400"></div>
     </div>
   );
